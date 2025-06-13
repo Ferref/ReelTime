080c6557f8f1e9a887ad2a74c720b8da53fc39b6
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-06-13 18:28:41 +0200

    Favicon and home page

---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyMovies</title>
+    <link rel="icon" type="image/x-icon" href="../popcorn.ico">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
