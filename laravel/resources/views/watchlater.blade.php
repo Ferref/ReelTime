8a133e6e906219503df9d439d636e25f08b84e1d
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-06-20 13:22:20 +0200

    Comments

--- laravel/resources/views/watchlater.blade.php
+++ laravel/resources/views/watchlater.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="watchlater-container my-5 container-fluid position-relative p-0 m-0 d-flex justify-content-center align-items-center h-100 flex-column">
-    {{!-- If no movies found --}}
+    {{-- If no movies found --}}
     <div class="d-none container-fluid movies-container py-5 d-flex h-100">
 
       <div class="row justify-content-center gap-3">
