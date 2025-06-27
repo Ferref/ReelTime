e02ae931f017c94896e44a36b7ff514f7dafabbb
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-06-27 14:18:10 +0200

    Pop out animation

--- laravel/resources/views/components/message.blade.php
+++ laravel/resources/views/components/message.blade.php
@@ -1,4 +1,4 @@
-<div class="messagetext container-fluid w-100 position-relative d-flex justify-content-center align-items-center py-4">
+<div class="message container-fluid w-100 position-relative d-flex justify-content-center align-items-center py-4">
     <div class="text-center text-light custom-dark p-4 rounded shadow">
         <h1 class="mb-2">No movies found on your watchlist...</h1>
         <p>Did you see these movies yet?</p>
